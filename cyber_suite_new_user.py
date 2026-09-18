4e450ea4522bb8e8d3a9fc2b3bdf29c499544d44
Lukekozitskysketch <lkozitsky@fingerlakes.edu>
2026-09-17 23:04:46 -0400

    Update cyber_suite_new_user.py

--- cyber_suite_new_user.py
+++ cyber_suite_new_user.py
@@ -1,1 +1,11 @@
+import getpass
+
+name = input("Please enter your name: ")
+user_id = input("Please enter your user id: ")
+password = getpass.getpass("Please enter your password: ")
+
+print(f"\nWelcome, {name}. Your ID is {user_id}.\n")
+
+masked_password = "X" * len(password)
+print(f"PASSWORD: {masked_password}")
 
